f738a8353aa1e76ad65c5023f337b8eaad0b94e7
Philip Teow <philipteow@philips-macbook-pro.local>
2018-02-08 16:24:42 +0800

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Philip Teow on 08/02/2018.
+//  Copyright © 2018 Philip Teow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
