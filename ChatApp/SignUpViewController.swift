25538c7b9ed89b1b6a6a3eff0549d300170ab3ba
cybotchua <cybotchua@hotmail.com>
2018-02-09 17:33:16 +0800

    add alert function

--- ChatApp/SignUpViewController.swift
+++ ChatApp/SignUpViewController.swift
@@ -38,7 +38,36 @@ class SignUpViewController: UIViewController {
         
         //Input Validation
         if !email.contains("@") {
-//            showAlert
+            showAlert(withTitle: "Invalid Email Format", message: "Please input a valid email")
+        } else if username.count < 3 {
+            showAlert(withTitle: "Invalid Username", message: "Username must contain at least 3 characters")
+        } else if password.count < 7 {
+            showAlert(withTitle: "Invalid Password", message: "Password must be at least 7 characters long")
+        } else if confirmPassword != password {
+            showAlert(withTitle: "Passwords Do Not Match", message: "Please enter the same passwords")
+        } else {
+            Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                
+                //Error
+                if let validError = error {
+                    self.showAlert(withTitle: "ERROR", message: validError.localizedDescription)
+                }
+                
+                
+                //Successful Creation of New User
+                if let validUser = user {
+                    //let 
+                }
+                
+                
+                
+            })
+            
+            
+            
+            
+            
+            
         }
         
         
